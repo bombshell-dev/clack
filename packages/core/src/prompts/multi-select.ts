a777e06474610d89a11001e5d988f4dcca7517db
Zihan Chen <me@zihan.ga>
2023-02-17 12:13:52 +0800

    Allow empty

--- packages/core/src/prompts/multi-select.ts
+++ packages/core/src/prompts/multi-select.ts
@@ -1,9 +1,10 @@
-import Prompt, { PromptOptions } from './prompt';
-import color from 'picocolors';
+import Prompt, { PromptOptions } from "./prompt";
+import color from "picocolors";
 
 interface MultiSelectOptions<T extends { value: any }> extends PromptOptions<MultiSelectPrompt<T>> {
     options: T[];
-    initialValue?: T['value'];
+    initialValue?: T["value"];
+    allowEmpty?: boolean;
 }
 export default class MultiSelectPrompt<T extends { value: any }> extends Prompt {
     options: T[];
@@ -11,52 +12,54 @@ export default class MultiSelectPrompt<T extends { value: any }> extends Prompt 
     selectedValues: T[];
 
     private get _value() {
-        return this.options[this.cursor]
+        return this.options[this.cursor];
     }
 
     private changeValue() {
-        const isValueAlreadySelected = this.selectedValues.some(value => value === this._value.value);
+        const isValueAlreadySelected = this.selectedValues.some((value) => value === this._value.value);
         if (isValueAlreadySelected) {
-            this.selectedValues = this.selectedValues.filter(value => value !== this._value.value)
+            this.selectedValues = this.selectedValues.filter((value) => value !== this._value.value);
         } else {
             this.selectedValues.push(this._value.value);
         }
     }
 
     constructor(opts: MultiSelectOptions<T>) {
-        if (!opts.validate) {
-            opts.validate = () => {
-                if (this.selectedValues.length === 0) return `Please select at least one option\n${color.reset(color.dim(`Press ${color.gray(color.bgWhite(color.inverse(' space ')))} to select, ${color.gray(color.bgWhite(color.inverse(' enter ')))} to submit`))}`
+        if ((opts.allowEmpty ?? true) !== true) {
+            if (!opts.validate) {
+                opts.validate = () => {
+                    if (this.selectedValues.length === 0) return `Please select at least one option\n${color.reset(color.dim(`Press ${color.gray(color.bgWhite(color.inverse(" space ")))} to select, ${color.gray(color.bgWhite(color.inverse(" enter ")))} to submit`))}`;
+                };
             }
         }
         super(opts, false);
-        this.once('finalize', () => {
+        this.once("finalize", () => {
             this.value = this.selectedValues;
-        })
+        });
         this.options = opts.options;
         this.cursor = this.options.findIndex(({ value }) => value === opts.initialValue);
-        this.selectedValues = []
+        this.selectedValues = [];
         if (this.cursor === -1) this.cursor = 0;
 
-        this.on('cursor', (key) => {
+        this.on("cursor", (key) => {
             switch (key) {
-                case 'left': 
-                case 'up': 
+                case "left":
+                case "up":
                     this.cursor = this.cursor === 0 ? this.options.length - 1 : this.cursor - 1;
                     break;
-                case 'down': 
-                case 'right':
+                case "down":
+                case "right":
                     this.cursor = this.cursor === this.options.length - 1 ? 0 : this.cursor + 1;
                     break;
-                case 'space':
+                case "space":
                     this.changeValue();
                     break;
-                case 'enter':
-                case 'return':
-                    this.state = 'submit';
+                case "enter":
+                case "return":
+                    this.state = "submit";
                     this.close();
                     break;
             }
-        })
+        });
     }
 }
