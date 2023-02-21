a82276cc2c7592a308fcbf36d9fcc55a7326ed51
Oskar Löfgren <516549+ulken@users.noreply.github.com>
2023-02-22 00:17:26 +0100

    case-insensitive checks

--- packages/core/src/prompts/select-key.ts
+++ packages/core/src/prompts/select-key.ts
@@ -11,12 +11,12 @@ export default class SelectKeyPrompt<T extends { value: any }> extends Prompt {
 		super(opts, false);
 
 		this.options = opts.options;
-		const keys = this.options.map(({ value: [initial] }) => initial);
+		const keys = this.options.map(({ value: [initial] }) => initial?.toLowerCase());
 		this.cursor = Math.max(keys.indexOf(opts.initialValue), 0);
 
 		this.on('key', (key) => {
 			if (!keys.includes(key)) return;
-			const value = this.options.find(({ value: [initial] }) => initial === key);
+			const value = this.options.find(({ value: [initial] }) => initial?.toLowerCase() === key);
 			if (value) {
 				this.value = value.value;
 				this.state = 'submit';
