20c19d9defaba33174b3f2bd8fb9bea0d1c009db
Bruno Rocha <brunorocha2674@gmail.com>
2023-08-10 18:39:57 -0300

    feat(@clack/prompts): builder example

---
+++ examples/builder.ts
@@ -0,0 +1,59 @@
+import * as p from '@clack/prompts';
+
+(async () => {
+	const results = await p
+		.builder()
+		.add('path', () =>
+			p.text({
+				message: 'Where should we create your project?',
+				placeholder: './sparkling-solid',
+				validate: (value) => {
+					if (!value) return 'Please enter a path.';
+					if (value[0] !== '.') return 'Please enter a relative path.';
+				},
+			})
+		)
+		.add('password', () =>
+			p.password({
+				message: 'Provide a password',
+				validate: (value) => {
+					if (!value) return 'Please enter a password.';
+					if (value.length < 5) return 'Password should have at least 5 characters.';
+				},
+			})
+		)
+		.add('type', ({ results }) =>
+			p.select({
+				message: `Pick a project type within "${results.path}"`,
+				initialValue: 'ts',
+				maxItems: 5,
+				options: [
+					{ value: 'ts', label: 'TypeScript' },
+					{ value: 'js', label: 'JavaScript' },
+					{ value: 'rust', label: 'Rust' },
+					{ value: 'go', label: 'Go' },
+					{ value: 'python', label: 'Python' },
+					{ value: 'coffee', label: 'CoffeeScript', hint: 'oh no' },
+				],
+			})
+		)
+		.add('tools', () =>
+			p.multiselect({
+				message: 'Select additional tools.',
+				initialValues: ['prettier', 'eslint'],
+				options: [
+					{ value: 'prettier', label: 'Prettier', hint: 'recommended' },
+					{ value: 'eslint', label: 'ESLint', hint: 'recommended' },
+					{ value: 'stylelint', label: 'Stylelint' },
+					{ value: 'gh-action', label: 'GitHub Action' },
+				],
+			})
+		)
+		.add('install', ({ results }) =>
+			p.confirm({
+				message: 'Install dependencies?',
+				initialValue: false,
+			})
+		)
+		.run();
+})();
