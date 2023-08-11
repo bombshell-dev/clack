c2a341cd0f64b88ffd6c94a4746b4d64e36d6a13
Christian Preston <christianpreston@ymail.com>
2023-08-11 14:04:18 -0400

    chore: fix tab space

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://clack.cc",
+	site: "https://clack.cc",
 	server: {
 		headers: {
 			'Cross-Origin-Embedder-Policy': 'require-corp',
