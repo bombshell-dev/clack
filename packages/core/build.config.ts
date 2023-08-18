3d692c7230b6837979c7f8994047dba5e9eb284f
Bruno Rocha <brunorocha2674@gmail.com>
2023-08-18 14:30:57 -0300

    feat: theme package

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -3,5 +3,14 @@ import { defineBuildConfig } from 'unbuild';
 // @see https://github.com/unjs/unbuild
 export default defineBuildConfig({
 	preset: '../../build.preset',
-	entries: ['src/index'],
+	entries: [
+		{
+			name: 'index',
+			input: 'src/prompts/index',
+		},
+		{
+			name: 'themes',
+			input: 'src/themes/index',
+		},
+	],
 });
