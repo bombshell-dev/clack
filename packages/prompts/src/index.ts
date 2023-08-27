fec6efb4698aeb030cf13962cf03b393c8d335e0
Artemy Egorov <artegoser@gmail.com>
2023-08-27 14:14:10 +0300

    feat(@clack/prompts): add tasks
    like listr

--- packages/prompts/src/index.ts
+++ packages/prompts/src/index.ts
@@ -759,3 +759,33 @@ export const group = async <T>(
 
 	return results;
 };
+
+export type Task = {
+	/**
+	 * Task title
+	 */
+	title: string;
+	/**
+	 * Task function
+	 */
+	task: (message: (string: string) => void) => string | Promise<string> | void | Promise<void>;
+
+	/**
+	 * If enabled === false the task will be skipped
+	 */
+	enabled?: boolean;
+}
+
+/**
+ * Define a group of tasks to be executed
+ */
+export const tasks = async (tasks: Task[]) => {
+	for (const task of tasks) {
+		if (task.enabled !== false) {
+			let s = spinner();
+			s.start(task.title);
+			const result = await task.task(s.message);
+			s.stop(result || task.title);
+		}
+	}
+}
