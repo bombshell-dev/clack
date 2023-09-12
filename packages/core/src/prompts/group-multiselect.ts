4c7ef30fd820a59b1dcb957fcfb2f986186f1201
Bruno Rocha <brunorocha2674@gmail.com>
2023-09-12 11:12:08 -0300

    test: GroupMultiSelectPrompt

--- packages/core/src/prompts/group-multiselect.ts
+++ packages/core/src/prompts/group-multiselect.ts
@@ -1,26 +1,28 @@
+import { exposeTestUtils } from '../utils';
 import Prompt, { PromptOptions } from './prompt';
 
-interface GroupMultiSelectOptions<T extends { value: any }>
+export interface GroupMultiSelectOptions<T extends { value: any }>
 	extends PromptOptions<GroupMultiSelectPrompt<T>> {
 	options: Record<string, T[]>;
 	initialValues?: T['value'][];
 	required?: boolean;
 	cursorAt?: T['value'];
 }
+
 export default class GroupMultiSelectPrompt<T extends { value: any }> extends Prompt {
-	options: (T & { group: string | boolean })[];
-	cursor: number = 0;
+	public options: (T & { group: string | boolean })[];
+	public cursor: number;
 
 	getGroupItems(group: string): T[] {
 		return this.options.filter((o) => o.group === group);
 	}
 
-	isGroupSelected(group: string) {
+	isGroupSelected(group: string): boolean {
 		const items = this.getGroupItems(group);
 		return items.every((i) => this.value.includes(i.value));
 	}
 
-	private toggleValue() {
+	private toggleValue(): void {
 		const item = this.options[this.cursor];
 		if (item.group === true) {
 			const group = item.value;
@@ -43,16 +45,30 @@ export default class GroupMultiSelectPrompt<T extends { value: any }> extends Pr
 
 	constructor(opts: GroupMultiSelectOptions<T>) {
 		super(opts, false);
+
 		const { options } = opts;
 		this.options = Object.entries(options).flatMap(([key, option]) => [
 			{ value: key, group: true, label: key },
 			...option.map((opt) => ({ ...opt, group: key })),
 		]) as any;
-		this.value = [...(opts.initialValues ?? [])];
+
+		this.value = [];
+		if (opts.initialValues) {
+			for (const value of opts.initialValues) {
+				const optionIndex = this.options.findIndex((o) => o.value === value);
+				if (optionIndex !== -1) {
+					this.cursor = optionIndex;
+					this.toggleValue();
+				}
+			}
+		}
+
 		this.cursor = Math.max(
 			this.options.findIndex(({ value }) => value === opts.cursorAt),
 			0
 		);
+
+		this.exposeTestUtils();
 
 		this.on('cursor', (key) => {
 			switch (key) {
@@ -68,6 +84,15 @@ export default class GroupMultiSelectPrompt<T extends { value: any }> extends Pr
 					this.toggleValue();
 					break;
 			}
+			this.exposeTestUtils();
+		});
+	}
+
+	private exposeTestUtils() {
+		exposeTestUtils<GroupMultiSelectPrompt<any>>({
+			options: this.options,
+			cursor: this.cursor,
+			value: this.value,
 		});
 	}
 }
