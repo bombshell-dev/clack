de97ffc942b1c2040c6dbac8e703a1dcdc7d760a
Bruno Rocha <brunorocha2674@gmail.com>
2023-09-12 12:21:55 -0300

    test: clack core exports

---
+++ packages/core/__tests__/index.spec.ts
@@ -0,0 +1,31 @@
+import { readdirSync } from 'node:fs';
+import * as packageExports from '../src/index';
+import { join } from 'node:path';
+
+describe('Package', () => {
+	it('should export all prompts', async () => {
+		const exportedPrompts = Object.keys(packageExports).filter((key) => /prompt/i.test(key) && !/^mock/.test(key));
+		const promptsPath = join(__dirname, '../src/prompts');
+		const promptFiles = readdirSync(promptsPath);
+
+		expect.assertions(promptFiles.length + 1);
+		for (const file of promptFiles) {
+			const prompt = await import(join(promptsPath, file));
+			expect(exportedPrompts).toContain(prompt.default.name);
+		}
+		expect(exportedPrompts).toHaveLength(promptFiles.length);
+	});
+
+	it('should export selected utils', async () => {
+		const exportedUtils = Object.keys(packageExports).filter(
+			(key) => !/prompt/i.test(key) || /^mock/.test(key)
+		);
+		const utils: string[] = ['block', 'isCancel', 'mockPrompt', 'setGlobalAliases'];
+
+		expect.assertions(utils.length + 1);
+		for (const util of utils) {
+			expect(exportedUtils).toContain(util);
+		}
+		expect(exportedUtils).toHaveLength(utils.length);
+	});
+});
