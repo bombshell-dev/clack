90d151a9cb070f48ddb7aa4dd7e56876a1a744f7
Bruno Rocha <brunorocha2674@gmail.com>
2023-09-12 11:55:04 -0300

    test: SelectKeyPrompt

--- packages/core/src/prompts/select-key.ts
+++ packages/core/src/prompts/select-key.ts
@@ -1,11 +1,14 @@
+import { exposeTestUtils } from '../utils';
 import Prompt, { PromptOptions } from './prompt';
 
-interface SelectKeyOptions<T extends { value: any }> extends PromptOptions<SelectKeyPrompt<T>> {
+export interface SelectKeyOptions<T extends { value: any }>
+	extends PromptOptions<SelectKeyPrompt<T>> {
 	options: T[];
 }
+
 export default class SelectKeyPrompt<T extends { value: any }> extends Prompt {
-	options: T[];
-	cursor: number = 0;
+	public options: T[];
+	public cursor: number;
 
 	constructor(opts: SelectKeyOptions<T>) {
 		super(opts, false);
@@ -13,6 +16,8 @@ export default class SelectKeyPrompt<T extends { value: any }> extends Prompt {
 		this.options = opts.options;
 		const keys = this.options.map(({ value: [initial] }) => initial?.toLowerCase());
 		this.cursor = Math.max(keys.indexOf(opts.initialValue), 0);
+
+		this.exposeTestUtils();
 
 		this.on('key', (key) => {
 			if (!keys.includes(key)) return;
@@ -22,6 +27,16 @@ export default class SelectKeyPrompt<T extends { value: any }> extends Prompt {
 				this.state = 'submit';
 				this.emit('submit');
 			}
+			this.exposeTestUtils();
+		});
+	}
+
+	private exposeTestUtils() {
+		exposeTestUtils<SelectKeyPrompt<any>>({
+			state: this.state,
+			options: this.options,
+			cursor: this.cursor,
+			value: this.value,
 		});
 	}
 }
