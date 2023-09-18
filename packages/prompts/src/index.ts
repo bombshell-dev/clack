febb4826f40cc806221ff12ea2cd0d102d425dcb
Bruno Rocha <brunorocha2674@gmail.com>
2023-09-18 14:19:40 -0300

    fix: ci

--- packages/prompts/src/index.ts
+++ packages/prompts/src/index.ts
@@ -1,19 +1,22 @@
 export { isCancel, mockPrompt, setGlobalAliases } from '@clack/core';
-export { ConfirmOptions, default as confirm } from './prompts/confirm';
+export { default as confirm, type ConfirmOptions } from './prompts/confirm';
 export {
 	default as group,
-	PromptGroup,
-	PromptGroupAwaitedReturn,
-	PromptGroupOptions
+	type PromptGroup,
+	type PromptGroupAwaitedReturn,
+	type PromptGroupOptions
 } from './prompts/group';
-export { default as groupMultiselect, GroupMultiSelectOptions } from './prompts/group-multiselect';
-export { cancel, default as log, intro, LogMessageOptions, outro } from './prompts/log';
-export { default as multiselect, MultiSelectOptions } from './prompts/multiselect';
+export {
+	default as groupMultiselect,
+	type GroupMultiSelectOptions
+} from './prompts/group-multiselect';
+export { cancel, default as log, intro, outro, type LogMessageOptions } from './prompts/log';
+export { default as multiselect, type MultiSelectOptions } from './prompts/multiselect';
 export { default as note } from './prompts/note';
-export { default as password, PasswordOptions } from './prompts/password';
+export { default as password, type PasswordOptions } from './prompts/password';
 export { default as select } from './prompts/select';
 export { default as selectKey } from './prompts/select-key';
 export { default as spinner } from './prompts/spinner';
-export { default as tasks, Task } from './prompts/tasks';
-export { default as text, TextOptions } from './prompts/text';
-export { Option, SelectOptions } from './utils';
+export { default as tasks, type Task } from './prompts/tasks';
+export { default as text, type TextOptions } from './prompts/text';
+export { type Option, type SelectOptions } from './utils';
