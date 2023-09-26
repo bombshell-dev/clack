a737c85082b7660aa3daad124646ca5cb724b477
Bruno Rocha <brunorocha2674@gmail.com>
2023-09-26 12:44:22 -0300

    chore: add spinner-ci description

--- examples/basic/spinner-ci.ts
+++ examples/basic/spinner-ci.ts
@@ -1,3 +1,14 @@
+/**
+ * This example addresses a issue reported in GitHub Actions where `spinner` was excessively writing messages,
+ * leading to confusion and cluttered output.
+ * To enhance the CI workflow and provide a smoother experience,
+ * the following changes have been made only for CI environment:
+ * - Messages will now only be written when a `spinner` method is called and the message updated, preventing unnecessary message repetition.
+ * - There will be no loading dots animation, instead it will be always `...`
+ * - Instead of erase the previous message, action that is blocked during CI, it will just write a new one.
+ *
+ * Issue: https://github.com/natemoo-re/clack/issues/168
+ */
 import * as p from '@clack/prompts';
 
 const s = p.spinner();
