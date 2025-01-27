e8772e09404cbe398201bddd5e6d83cf47b28e3a
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-01-27 16:56:32 +0100

    .

--- examples/basic/spinner-timer.ts
+++ examples/basic/spinner-timer.ts
@@ -2,16 +2,25 @@ import * as p from '@clack/prompts';
 
 p.intro('spinner start...');
 
-const spin = p.spinner({ indicator: 'timer' });
-const totalDuration = 10_000;
+async function main() {
+	const spin = p.spinner({ indicator: 'timer' });
 
-spin.start('Loading');
+	spin.start('First spinner');
 
-new Promise((resolve) => {
-	setTimeout(() => {
-		resolve(true);
-	}, totalDuration);
-}).then(() => {
-	spin.stop('Done');
-	p.outro('spinner stop...');
-});
+	await sleep(3_000);
+
+	spin.stop('Done first spinner');
+
+	spin.start('Second spinner');
+	await sleep(5_000);
+
+	spin.stop('Done second spinner');
+
+	p.outro('spinner stop.');
+}
+
+function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main();
