6c6511366a26656cc27e2be82bc63ead0225d9dd
Nate Moore <nate@natemoo.re>
2025-02-04 22:50:13 -0600

    feat(prompts): add `stream` API

--- packages/prompts/src/index.ts
+++ packages/prompts/src/index.ts
@@ -675,6 +675,48 @@ export const log = {
 	},
 };
 
+const prefix = `${color.gray(S_BAR)}  `;
+export const stream = {
+	message: async (iterable: Iterable<string>|AsyncIterable<string>, { symbol = color.gray(S_BAR) }: LogMessageOptions = {}) => {
+		process.stdout.write(`${color.gray(S_BAR)}\n${symbol}  `);
+		let lineWidth = 3;
+		for await (let chunk of iterable) {
+			chunk = chunk.replace(/\n/g, `\n${prefix}`);
+			if (chunk.includes('\n')) {
+				lineWidth = 3 + strip(chunk.slice(chunk.lastIndexOf('\n'))).length;
+			}
+			const chunkLen = strip(chunk).length;
+			if ((lineWidth + chunkLen) < process.stdout.columns) {
+				lineWidth += chunkLen;
+				process.stdout.write(chunk);
+			} else {
+				process.stdout.write(`\n${prefix}${chunk.trimStart()}`);
+				lineWidth = 3 + strip(chunk.trimStart()).length;
+			}
+		}
+		process.stdout.write('\n');
+	},
+	info: (iterable: Iterable<string>|AsyncIterable<string>) => {
+		return stream.message(iterable, { symbol: color.blue(S_INFO) });
+	},
+	success: (iterable: Iterable<string>|AsyncIterable<string>) => {
+		return stream.message(iterable, { symbol: color.green(S_SUCCESS) });
+	},
+	step: (iterable: Iterable<string>|AsyncIterable<string>) => {
+		return stream.message(iterable, { symbol: color.green(S_STEP_SUBMIT) });
+	},
+	warn: (iterable: Iterable<string>|AsyncIterable<string>) => {
+		return stream.message(iterable, { symbol: color.yellow(S_WARN) });
+	},
+	/** alias for `log.warn()`. */
+	warning: (iterable: Iterable<string>|AsyncIterable<string>) => {
+		return stream.warn(iterable);
+	},
+	error: (iterable: Iterable<string>|AsyncIterable<string>) => {
+		return stream.message(iterable, { symbol: color.red(S_ERROR) });
+	},
+}
+
 export const spinner = () => {
 	const frames = unicode ? ['◒', '◐', '◓', '◑'] : ['•', 'o', 'O', '0'];
 	const delay = unicode ? 80 : 120;
