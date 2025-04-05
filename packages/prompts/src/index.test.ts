a8fa02bfd2a963909f049c2537b85d39fdc19cd6
James Garbutt <43081j@users.noreply.github.com>
2025-04-05 15:41:31 +0100

    test: add tests for group multiselect

--- packages/prompts/src/index.test.ts
+++ packages/prompts/src/index.test.ts
@@ -899,4 +899,286 @@ describe.each(['true', 'false'])('prompts (isCI = %s)', (isCI) => {
 			expect(output.buffer).toMatchSnapshot();
 		});
 	});
+
+	describe('groupMultiselect', () => {
+		test('renders message with options', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+					group2: [{ value: 'group2value0' }],
+				},
+			});
+
+			// Select the first non-group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+
+			// submit
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value0']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('can select multiple options', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }, { value: 'group1value2' }],
+				},
+			});
+
+			// Select the first non-group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+			// Select the second non-group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+
+			// submit
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value0', 'group1value1']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('can select a group', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+			});
+
+			// Select the group as a whole
+			input.emit('keypress', '', { name: 'space' });
+
+			// submit
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value0', 'group1value1']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('can select a group by selecting all members', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+			});
+
+			// Select the first group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+			// Select the second group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+
+			// submit
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value0', 'group1value1']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('can deselect an option', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+			});
+
+			// Select the first group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+			// Select the second group option
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+			// Deselect it
+			input.emit('keypress', '', { name: 'space' });
+
+			// submit
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value0']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('renders error when nothing selected', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+			});
+
+			// try submit
+			input.emit('keypress', '', { name: 'return' });
+			// now select something and submit
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value0']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		describe('selectableGroups = false', () => {
+			test('cannot select groups', async () => {
+				const result = prompts.groupMultiselect({
+					message: 'foo',
+					input,
+					output,
+					options: {
+						group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+					},
+					selectableGroups: false,
+				});
+
+				// first selectable item should be group's child
+				input.emit('keypress', '', { name: 'space' });
+				input.emit('keypress', '', { name: 'return' });
+
+				const value = await result;
+
+				expect(value).toEqual(['group1value0']);
+				expect(output.buffer).toMatchSnapshot();
+			});
+
+			test('selecting all members of group does not select group', async () => {
+				const result = prompts.groupMultiselect({
+					message: 'foo',
+					input,
+					output,
+					options: {
+						group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+					},
+					selectableGroups: false,
+				});
+
+				// first selectable item should be group's child
+				input.emit('keypress', '', { name: 'space' });
+				// select second item
+				input.emit('keypress', '', { name: 'down' });
+				input.emit('keypress', '', { name: 'space' });
+				// submit
+				input.emit('keypress', '', { name: 'return' });
+
+				const value = await result;
+
+				expect(value).toEqual(['group1value0', 'group1value1']);
+				expect(output.buffer).toMatchSnapshot();
+			});
+		});
+
+		test('can submit empty selection when require = false', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+				required: false,
+			});
+
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual([]);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('cursorAt sets initial selection', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+				cursorAt: 'group1value1',
+			});
+
+			input.emit('keypress', '', { name: 'space' });
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value1']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('initial values can be set', async () => {
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [{ value: 'group1value0' }, { value: 'group1value1' }],
+				},
+				initialValues: ['group1value1'],
+			});
+
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual(['group1value1']);
+			expect(output.buffer).toMatchSnapshot();
+		});
+
+		test('values can be non-primitive', async () => {
+			const value0 = Symbol();
+			const value1 = Symbol();
+			const result = prompts.groupMultiselect({
+				message: 'foo',
+				input,
+				output,
+				options: {
+					group1: [
+						{ value: value0, label: 'value0' },
+						{ value: value1, label: 'value1' },
+					],
+				},
+			});
+
+			input.emit('keypress', '', { name: 'down' });
+			input.emit('keypress', '', { name: 'space' });
+			input.emit('keypress', '', { name: 'return' });
+
+			const value = await result;
+
+			expect(value).toEqual([value0]);
+			expect(output.buffer).toMatchSnapshot();
+		});
+	});
 });
