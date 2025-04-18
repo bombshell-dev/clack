7a08f3d3c63602c26edcd14c2a781cfc82ea3694
43081j <5677153+43081j@users.noreply.github.com>
2025-04-18 11:36:21 +0000

    [ci] format

--- packages/prompts/vitest.config.ts
+++ packages/prompts/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		snapshotSerializers: ['vitest-ansi-serializer']
+		snapshotSerializers: ['vitest-ansi-serializer'],
 	},
 });
