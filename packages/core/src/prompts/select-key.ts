df35f2608aa41a985a603ce74ecab7a960d97d28
James Garbutt <43081j@users.noreply.github.com>
2025-05-27 15:11:23 +0100

    fix: only accept strings in selectKey

--- packages/core/src/prompts/select-key.ts
+++ packages/core/src/prompts/select-key.ts
@@ -1,10 +1,10 @@
 import Prompt, { type PromptOptions } from './prompt.js';
 
-interface SelectKeyOptions<T extends { value: any }>
+interface SelectKeyOptions<T extends { value: string }>
 	extends PromptOptions<T['value'], SelectKeyPrompt<T>> {
 	options: T[];
 }
-export default class SelectKeyPrompt<T extends { value: any }> extends Prompt<T['value']> {
+export default class SelectKeyPrompt<T extends { value: string }> extends Prompt<T['value']> {
 	options: T[];
 	cursor = 0;
 
@@ -16,7 +16,7 @@ export default class SelectKeyPrompt<T extends { value: any }> extends Prompt<T[
 		this.cursor = Math.max(keys.indexOf(opts.initialValue), 0);
 
 		this.on('key', (key) => {
-			if (!keys.includes(key)) return;
+			if (!key || !keys.includes(key)) return;
 			const value = this.options.find(({ value: [initial] }) => initial?.toLowerCase() === key);
 			if (value) {
 				this.value = value.value;
