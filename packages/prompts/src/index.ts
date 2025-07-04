756b3e358524f8d32da62e1a274b8a369e46d162
hyperz111 <firzeroun11@gmail.com>
2025-07-04 15:15:13 +0700

    Change src/index.ts

--- packages/prompts/src/index.ts
+++ packages/prompts/src/index.ts
@@ -1,22 +1,25 @@
 export { isCancel, updateSettings, settings, type ClackSettings } from '@clack/core';
 
-export * from './autocomplete.js';
 export * from './common.js';
-export * from './confirm.js';
-export * from './group-multi-select.js';
-export * from './group.js';
 export * from './limit-options.js';
-export * from './log.js';
-export * from './messages.js';
-export * from './multi-select.js';
-export * from './note.js';
-export * from './password.js';
-export * from './path.js';
-export * from './progress-bar.js';
-export * from './select-key.js';
-export * from './select.js';
-export * from './spinner.js';
-export * from './stream.js';
-export * from './task.js';
-export * from './task-log.js';
-export * from './text.js';
+
+export * from './prompts/autocomplete.js';
+export * from './prompts/confirm.js';
+export * from './prompts/group-multi-select.js';
+export * from './prompts/multi-select.js';
+export * from './prompts/password.js';
+export * from './prompts/path.js';
+export * from './prompts/select-key.js';
+export * from './prompts/select.js';
+export * from './prompts/text.js';
+
+export * from './components/log.js';
+export * from './components/messages.js';
+export * from './components/note.js';
+export * from './components/progress-bar.js';
+export * from './components/spinner.js';
+export * from './components/group.js';
+
+export * from './utils/stream.js';
+export * from './utils/task.js';
+export * from './utils/task-log.js';
