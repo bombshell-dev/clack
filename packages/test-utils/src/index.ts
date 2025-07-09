4d639194059b87726244be008d0bbb2aa9d3d97d
James Garbutt <43081j@users.noreply.github.com>
2025-07-09 16:40:06 +0100

    wip: add form tests

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,2 +1,8 @@
 export { MockReadable } from './mock-readable.js';
 export { MockWritable } from './mock-writable.js';
+
+export function nextTick(): Promise<void> {
+	return new Promise<void>((resolve) => {
+		setTimeout(resolve, 0);
+	});
+}
