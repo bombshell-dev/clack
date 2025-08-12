c4bbe7d9257424836c57254628cd6a043772e65c
hyperz111 <firzeroun11@gmail.com>
2025-08-13 04:46:21 +0700

    fix: fix overflow note component

--- packages/prompts/src/note.ts
+++ packages/prompts/src/note.ts
@@ -10,6 +10,8 @@ import {
 	S_CORNER_TOP_RIGHT,
 	S_STEP_SUBMIT,
 } from './common.js';
+import { wrapAnsi } from "fast-wrap-ansi";
+import process from "node:process";
 
 export interface NoteOptions extends CommonOptions {
 	format?: (line: string) => string;
@@ -18,26 +20,23 @@ export interface NoteOptions extends CommonOptions {
 const defaultNoteFormatter = (line: string): string => color.dim(line);
 
 export const note = (message = '', title = '', opts?: NoteOptions) => {
-	const format = opts?.format ?? defaultNoteFormatter;
-	const lines = ['', ...message.split('\n').map(format), ''];
-	const titleLen = strip(title).length;
-	const output: Writable = opts?.output ?? process.stdout;
-	const len =
-		Math.max(
-			lines.reduce((sum, ln) => {
-				const line = strip(ln);
-				return line.length > sum ? line.length : sum;
-			}, 0),
-			titleLen
-		) + 2;
-	const msg = lines
-		.map(
-			(ln) => `${color.gray(S_BAR)}  ${ln}${' '.repeat(len - strip(ln).length)}${color.gray(S_BAR)}`
-		)
-		.join('\n');
-	output.write(
-		`${color.gray(S_BAR)}\n${color.green(S_STEP_SUBMIT)}  ${color.reset(title)} ${color.gray(
-			S_BAR_H.repeat(Math.max(len - titleLen - 1, 1)) + S_CORNER_TOP_RIGHT
-		)}\n${msg}\n${color.gray(S_CONNECT_LEFT + S_BAR_H.repeat(len + 2) + S_CORNER_BOTTOM_RIGHT)}\n`
-	);
+  const output: Writable = opts?.output ?? process.stdout;
+  const format = opts?.format ?? defaultNoteFormatter;
+  const wrapMsg = wrapAnsi(`\n${message}\n`, process.stdout.columns - 6); 
+  const titleLen = strip(title).length;
+  const lines = wrapMsg.split('\n');
+  const len = Math.max(
+    lines.reduce((sum, ln) => {
+      const line = strip(ln); 
+      return line.length > sum ? line.length : sum;
+    }, 0),
+    titleLen
+   );
+  const header = `${color.green(S_STEP_SUBMIT)}  ${color.reset(title)} ${color.gray(S_BAR_H.repeat(len - titleLen + 1) + S_CORNER_TOP_RIGHT)}`;
+  const noteLines = lines.map(
+    (line) =>
+      `${color.gray(S_BAR)}  ${format(line)}${' '.repeat(len - strip(line).length + 2)}${color.gray(S_BAR)}`
+  );
+  const footer = `${color.gray(S_CONNECT_LEFT + S_BAR_H.repeat(len + 4) + S_CORNER_BOTTOM_RIGHT)}`;
+  output.write(`${color.gray(S_BAR)}\n${header}\n${noteLines.join('\n')}\n${footer}\n`);
 };
