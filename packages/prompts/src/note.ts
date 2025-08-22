c718cc69b17008f722343e1ef01eb745eef0c370
hyperz111 <firzeroun11@gmail.com>
2025-08-22 18:08:21 +0700

    some refactor

--- packages/prompts/src/note.ts
+++ packages/prompts/src/note.ts
@@ -20,28 +20,35 @@ export interface NoteOptions extends CommonOptions {
 const defaultNoteFormatter = (line: string): string => color.dim(line);
 
 const wrapWithFormat = (message: string, width: number, format: NoteOptions["format"]): string => {
-  const wrapMsg = wrapAnsi(message, width).split("\n");
-  const maxWidthNormal = wrapMsg.reduce((sum, ln) => Math.max(strip(ln).length, sum), 0);
-  const maxWidthFormat = wrapMsg.map(format).reduce((sum, ln) => Math.max(strip(ln).length, sum), 0);
-  const wrapWidth = width - (maxWidthFormat - maxWidthNormal);
-  return wrapAnsi(message, wrapWidth);
+	const wrapMsg = wrapAnsi(message, width).split("\n");
+	const maxWidthNormal = wrapMsg.reduce((sum, ln) => Math.max(strip(ln).length, sum), 0);
+	const maxWidthFormat = wrapMsg.map(format).reduce((sum, ln) => Math.max(strip(ln).length, sum), 0);
+	const wrapWidth = width - (maxWidthFormat - maxWidthNormal);
+	return wrapAnsi(message, wrapWidth);
 }
 
 export const note = (message = '', title = '', opts?: NoteOptions) => {
-  const output: Writable = opts?.output ?? process.stdout;
-  const format = opts?.format ?? defaultNoteFormatter;
-  const wrapMsg = wrapWithFormat(message, output.columns - 6, format);
-  const lines = ['', ...wrapMsg.split('\n').map(format), ''];
-  const titleLen = strip(title).length;
-  const len = Math.max(
-    lines.reduce((sum, ln) => Math.max(strip(ln).length, sum), 0),
-    titleLen
-  );
-  const header = `${color.green(S_STEP_SUBMIT)}  ${color.reset(title)} ${color.gray(S_BAR_H.repeat(len - titleLen + 1) + S_CORNER_TOP_RIGHT)}`;
-  const noteLines = lines.map(
-    (line) =>
-      `${color.gray(S_BAR)}  ${line}${' '.repeat(len - strip(line).length + 2)}${color.gray(S_BAR)}`
-  );
-  const footer = `${color.gray(S_CONNECT_LEFT + S_BAR_H.repeat(len + 4) + S_CORNER_BOTTOM_RIGHT)}`;
-  output.write(`${color.gray(S_BAR)}\n${header}\n${noteLines.join('\n')}\n${footer}\n`);
+	const output: Writable = opts?.output ?? process.stdout;
+	const format = opts?.format ?? defaultNoteFormatter;
+	const wrapMsg = wrapWithFormat(message, output.columns - 6, format);
+	const lines = ['', ...wrapMsg.split('\n').map(format), ''];
+	const titleLen = strip(title).length;
+	const len =
+		Math.max(
+			lines.reduce((sum, ln) => {
+				const line = strip(ln);
+				return line.length > sum ? line.length : sum;
+			}, 0),
+			titleLen
+		) + 2;
+	const msg = lines
+		.map(
+			(ln) => `${color.gray(S_BAR)}  ${ln}${' '.repeat(len - strip(ln).length)}${color.gray(S_BAR)}`
+		)
+		.join('\n');
+	output.write(
+		`${color.gray(S_BAR)}\n${color.green(S_STEP_SUBMIT)}  ${color.reset(title)} ${color.gray(
+			S_BAR_H.repeat(len - titleLen + 3) + S_CORNER_TOP_RIGHT
+		)}\n${msg}\n${color.gray(S_CONNECT_LEFT + S_BAR_H.repeat(len + 2) + S_CORNER_BOTTOM_RIGHT)}\n`
+	);
 };
