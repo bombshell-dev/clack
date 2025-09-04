655f66a1cd1aa92136bdac12f889c1d50064357a
hyperz111 <firzeroun11@gmail.com>
2025-09-05 06:07:31 +0700

    remove strict format function

--- packages/prompts/src/note.ts
+++ packages/prompts/src/note.ts
@@ -37,9 +37,9 @@ const wrapWithFormat = (message: string, width: number, format: FormatFn): strin
 
 export const note = (message = '', title = '', opts?: NoteOptions) => {
 	const output: Writable = opts?.output ?? process.stdout;
-  const format = (ln: string) => opts?.format?.(ln) ?? defaultNoteFormatter(ln);
+	const format = opts?.format ?? defaultNoteFormatter;
 	const wrapMsg = wrapWithFormat(message, getColumns(output) - 6, format);
-  const lines = ['', ...wrapMsg.split('\n').map(format), ''];
+	const lines = ['', ...wrapMsg.split('\n').map(format), ''];
 	const titleLen = strip(title).length;
 	const len =
 		Math.max(
