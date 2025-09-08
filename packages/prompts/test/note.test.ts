d62e1ed6f8d409d38ae9ed13f633451a3ddf87ad
hyperz111 <firzeroun11@gmail.com>
2025-09-08 17:26:58 +0700

    update tests

--- packages/prompts/test/note.test.ts
+++ packages/prompts/test/note.test.ts
@@ -66,6 +66,7 @@ describe.each(['true', 'false'])('note (isCI = %s)', (isCI) => {
 
 	test("don't overflow", () => {
 		const message = `${'test string '.repeat(32)}\n`.repeat(4).trim();
+		output.columns = 75;
 		prompts.note(message, 'title', {
 			input,
 			output,
@@ -76,6 +77,7 @@ describe.each(['true', 'false'])('note (isCI = %s)', (isCI) => {
 
 	test("don't overflow with formatter", () => {
 		const message = `${'test string '.repeat(32)}\n`.repeat(4).trim();
+		output.columns = 75;
 		prompts.note(message, 'title', {
 			format: (line) => colors.red(`* ${colors.cyan(line)} *`),
 			input,
@@ -90,9 +92,10 @@ describe.each(['true', 'false'])('note (isCI = %s)', (isCI) => {
 			'이게 첫 번째 줄이에요',
 			'これは次の行です',
 		];
+		output.columns = 10;
 		prompts.note(messages.join("\n"), '这是标题', {
 			input,
-			output: Object.assign(output, { columns: 10 }),
+			output,
 		});
 
 		expect(output.buffer).toMatchSnapshot();
@@ -103,10 +106,11 @@ describe.each(['true', 'false'])('note (isCI = %s)', (isCI) => {
 			'이게 첫 번째 줄이에요',
 			'これは次の行です',
 		];
+		output.columns = 10;
 		prompts.note(messages.join("\n"), '这是标题', {
 			format: (line) => colors.red(`* ${colors.cyan(line)} *`),
 			input,
-			output: Object.assign(output, { columns: 10 }),
+			output,
 		});
 
 		expect(output.buffer).toMatchSnapshot();
