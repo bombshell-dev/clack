d9f86eb2edafc7a9fb29718b35306e50670dc2b1
James Garbutt <43081j@users.noreply.github.com>
2025-10-01 17:58:05 +0100

    fix: change find cursor to use +/- 1

--- packages/core/src/utils/cursor.ts
+++ packages/core/src/utils/cursor.ts
@@ -8,7 +8,7 @@ export function findCursor<T extends { disabled?: boolean }>(
 	const clampedCursor = newCursor < 0 ? maxCursor : newCursor > maxCursor ? 0 : newCursor;
 	const newOption = options[clampedCursor];
 	if (newOption.disabled) {
-		return findCursor(clampedCursor, delta + (delta < 0 ? -1 : 1), options);
+		return findCursor(clampedCursor, (delta < 0 ? -1 : 1), options);
 	}
 	return clampedCursor;
 }
