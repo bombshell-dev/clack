2f5cf54a8f3a273cf533565e6470153721c5456d
Hyper-Z11 <firzeroun11@gmail.com>
2026-02-15 15:03:10 +0700

    feat: add editor core prompt

---
+++ packages/core/src/prompts/editor.ts
@@ -0,0 +1,41 @@
+import { spawnSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+import { readFileSync, rmSync, writeFileSync } from 'node:fs';
+import { tmpdir } from 'node:os';
+import { join } from 'node:path';
+import Prompt, { type PromptOptions } from './prompt.js';
+
+export interface EditorOptions extends PromptOptions<string, EditorPrompt> {
+	exec?: string;
+	postfix?: string;
+}
+
+export default class EditorPrompt extends Prompt<string> {
+	exec: string | undefined;
+	path: string | undefined;
+
+	private create() {
+		spawnSync(this.exec as string, [this.path as string]);
+		this.value = readFileSync(this.path as string, 'utf8');
+	}
+
+	constructor(opts: EditorOptions) {
+		super(opts, false);
+		this.value = opts.initialValue ?? '';
+
+		this.exec =
+			opts.exec ?? process.env.EDITOR ?? (process.platform === 'win32' ? 'notepad' : 'nano');
+		this.path = join(tmpdir(), `ce-${randomUUID()}${opts.postfix ?? ''}`);
+		writeFileSync(this.path, this.value as string);
+
+		this.on('key', (_char, key) => {
+			if (key.name === 'return') {
+				this.create();
+			}
+		});
+
+		this.on('finalize', () => {
+			rmSync(this.path as string);
+		});
+	}
+}
