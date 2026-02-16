c443fa44b4d0ba871577111b425a49977bc83aa3
Hyper-Z11 <firzeroun11@gmail.com>
2026-02-16 10:30:39 +0700

    feat: add & change some option

--- packages/core/src/prompts/editor.ts
+++ packages/core/src/prompts/editor.ts
@@ -6,27 +6,30 @@ import { join } from 'node:path';
 import Prompt, { type PromptOptions } from './prompt.js';
 
 export interface EditorOptions extends PromptOptions<string, EditorPrompt> {
-	exec?: string;
+	bin?: string;
+	args?: (path: string) => Array<string>;
 	postfix?: string;
 	tmpdir?: string;
 }
 
 export default class EditorPrompt extends Prompt<string> {
-	exec: string | undefined;
-	path: string | undefined;
+	bin!: string;
+	args!: Array<string>;
+	path!: string;
 
 	private create() {
-		spawnSync(this.exec as string, [this.path as string]);
-		this.value = readFileSync(this.path as string, 'utf8');
+		spawnSync(this.bin, this.args);
+		this.value = readFileSync(this.path, 'utf8');
 	}
 
 	constructor(opts: EditorOptions) {
 		super(opts, false);
 		this.value = opts.initialValue ?? '';
 
-		this.exec =
-			opts.exec ?? process.env.EDITOR ?? (process.platform === 'win32' ? 'notepad' : 'nano');
+		this.bin =
+			opts.bin ?? process.env.EDITOR ?? (process.platform === 'win32' ? 'notepad' : 'nano');
 		this.path = join(opts.tmpdir ?? tmpdir(), `ce-${randomUUID()}${opts.postfix ?? ''}`);
+		this.args = opts?.args?.(this.path) ?? [this.path];
 		writeFileSync(this.path, this.value as string);
 
 		this.on('key', (_char, key) => {
@@ -36,7 +39,7 @@ export default class EditorPrompt extends Prompt<string> {
 		});
 
 		this.on('finalize', () => {
-			rmSync(this.path as string);
+			rmSync(this.path);
 		});
 	}
 }
