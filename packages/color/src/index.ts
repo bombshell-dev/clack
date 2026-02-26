d5d993c4e875056e2125f6132f2cdec9928af482
Gautier Ben Aim <benaim.gautier@gmail.com>
2026-02-26 22:05:41 +0100

    run biome

--- packages/color/src/index.ts
+++ packages/color/src/index.ts
@@ -1,21 +1,16 @@
-import { type InspectColor, styleText } from "node:util";
+import { type InspectColor, styleText } from 'node:util';
 
-export type StyleText = ((
-	text: string | TemplateStringsArray,
-	...args: unknown[]
-) => string) & { [key in InspectColor]: StyleText };
+export type StyleText = ((text: string | TemplateStringsArray, ...args: unknown[]) => string) & {
+	[key in InspectColor]: StyleText;
+};
 
 export function createStyleText(...styles: InspectColor[]): StyleText {
 	return new Proxy(
 		(text: string | TemplateStringsArray, ...args: unknown[]) =>
-			styleText(
-				styles,
-				typeof text === "string" ? text : String.raw({ raw: text }, ...args)
-			),
+			styleText(styles, typeof text === 'string' ? text : String.raw({ raw: text }, ...args)),
 		{
 			get(_, prop) {
-				if (typeof prop !== "string")
-					throw new TypeError("Property must be a string");
+				if (typeof prop !== 'string') throw new TypeError('Property must be a string');
 				return createStyleText(...styles, prop as InspectColor);
 			},
 		}
--- packages/color/test/color.test.ts
+++ packages/color/test/color.test.ts
@@ -1,19 +1,17 @@
-import c, { createStyleText } from "@clack/color";
-import { styleText } from "node:util";
-import { test, expect } from "vitest";
+import c, { createStyleText } from '@clack/color';
+import { styleText } from 'node:util';
+import { test, expect } from 'vitest';
 
-process.env.FORCE_COLOR = "1";
+process.env.FORCE_COLOR = '1';
 
-test("@clack/color", () => {
-	expect(c.redBright.underline("Hello World")).toBe(
-		styleText(["redBright", "underline"], "Hello World")
+test('@clack/color', () => {
+	expect(c.redBright.underline('Hello World')).toBe(
+		styleText(['redBright', 'underline'], 'Hello World')
 	);
 
-	expect(c.bgCyan.bold`Hello World`).toBe(
-		styleText(["bgCyan", "bold"], "Hello World")
-	);
+	expect(c.bgCyan.bold`Hello World`).toBe(styleText(['bgCyan', 'bold'], 'Hello World'));
 
-	expect(createStyleText("red", "inverse")(`Hello World`)).toBe(
-		styleText(["red", "inverse"], "Hello World")
+	expect(createStyleText('red', 'inverse')(`Hello World`)).toBe(
+		styleText(['red', 'inverse'], 'Hello World')
 	);
 });
