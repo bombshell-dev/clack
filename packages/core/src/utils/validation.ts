3f77e8ad73a363dea57f443f59a326d266dde468
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-05-20 16:51:43 +0200

    inline standard schema

--- packages/core/src/utils/validation.ts
+++ packages/core/src/utils/validation.ts
@@ -1,4 +1,4 @@
-import type { StandardSchemaV1 } from '@standard-schema/spec';
+import type { StandardSchemaV1 } from './standard-schema.js';
 
 /**
  * Represents the `validate()` option. A function or a
