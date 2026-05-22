0f1fcebd170460e422c1bd6f89d171e49660b982
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-05-22 09:41:20 +0200

    Update packages/core/src/prompts/prompt.ts
    Co-authored-by: Willow (GHOST) <ghostdevbusiness@gmail.com>

--- packages/core/src/prompts/prompt.ts
+++ packages/core/src/prompts/prompt.ts
@@ -23,8 +23,8 @@ export interface PromptOptions<TValue, Self extends Prompt<TValue>> {
 
 	/**
 	 * A function or a [Standard Schema](https://github.com/standard-schema/standard-schema)
-	 * that validates user input. Return a `string` or `Error` to show as a validation error,
-	 * or `undefined` to accept the result.
+	 * that validates user input. If a custom function is given, you should return a `string` or `Error`
+	 * to show as a validation error, or `undefined` to accept the result.
 	 */
 	validate?: Validate<TValue> | undefined;
 	input?: Readable;
